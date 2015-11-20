26e5e6504aaddcc03e244fc624221d1e21f8bfc4
DB <davidbochetto@wireless-s0-so-150-42-24.uchicago.edu>
2015-11-20 15:02:46 -0600

    likes, dislikes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,13 @@ class UsersController < ApplicationController
 
   	def show
   		@user = User.find(params[:id])
+  		@comment = Comment.new
 	end
 
 	def likes
 		@user = current_user
 		@likes = @user.liked_photos
+		@comment = Comment.new
 	end
 
 end
